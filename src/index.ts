3e287073944e5773501422fcf63dfcfbc6505e25
KIKW12 <ayala.zapata.juan.enrique123@gmail.com>
2024-10-08 17:16:28 -0600

    Feat: update package and tsconfig

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+import express from "express";
+
+//Para poder acceder a las variables del ambiente (.env)
+config();
+
+const app = express();
+
+app.listen(process.env.SERVER_PORT, function () {
+  console.log(`Server running on port ${process.env.SERVER_PORT}`);
+});
